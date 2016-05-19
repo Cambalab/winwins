30fa57a14fc0e73bac4ba40479ab695642ae3c4d
Alina Ryba <alinaryba@gmail.com>
2016-05-19 17:03:01 -0300

    Interna Contacto

---
+++ frontend/src/app/contact/contact.controller.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+  angular
+    .module('winwins')
+    .controller('ContactController', ContactController);
+
+  /** @ngInject */
+  function ContactController() {
+  }
+
+})();
