b46e407f30bf8038bf15cc65469dc858a1890c17
Clodo <claudiobidau@gmail.com>
2016-06-01 12:32:20 -0300

    clean seeds, sponsors

--- backend/database/seeds/SponsorsTableSeeder.php
+++ backend/database/seeds/SponsorsTableSeeder.php
@@ -10,98 +10,196 @@ class SponsorsTableSeeder extends Seeder {
         DB::table('sponsors')->delete();
 
         $sponsor = new Sponsor();
-        $sponsor->name = 'Aguas';
+        $sponsor->name = 'Perian SA';
         $sponsor->user_id = 1;
-        $sponsor->about = 'Aguas';
-        $sponsor->contact_name = 'José';
-        $sponsor->contact_phone = '111111111111';
-        $sponsor->contact_email = 'info@cocacola.com';
-        $sponsor->type = 'Industria - Comercial';
-        $sponsor->cover_photo = 'sponsor_9d83c2ef2129b23a653277f57cfd9a1d_142.jpeg';
+        $sponsor->about = 'Perian SA';
+        $sponsor->contact_name = 'Claudio';
+        $sponsor->contact_phone = '+541143816792';
+        $sponsor->contact_email = 'clodo@camba.coop';
+        $sponsor->type = 'Comercial';
+        $sponsor->cover_photo = 'perian-sponsor.png';
         $sponsor->is_active = 1;
         $sponsor->is_main = 1;
         $sponsor->status = 'ACTIVE';
-        $sponsor->photo = 'sponsor_9d83c2ef2129b23a653277f57cfd9a1d_142.jpeg';
+        $sponsor->photo = 'perian-sponsor.png';
 
         $sponsor->save();
-
-/*
-        $sponsors_user = new SponsorsUser();
-        $sponsors_user->user_id = 3;
-        $sponsors_user->sponsor_id = 1;
-        $sponsors_user->save();
-
-        $sponsors_winwins = new SponsorsWinwin();
-        $sponsors_winwins->winwin_id = 1;
-        $sponsors_winwins->sponsor_id = 1;
-        $sponsors_winwins->ww_message = 'Me aceptas?';
-        $sponsors_winwins->ww_accept = 1;
-        $sponsors_winwins->sponsor_accept = 1;
-        $sponsors_winwins->order = 1;
-        $sponsors_winwins->save();
-
-        $sponsors_winwins = new SponsorsWinwin();
-        $sponsors_winwins->winwin_id = 2;
-        $sponsors_winwins->sponsor_id = 1;
-        $sponsors_winwins->sponsor_message = 'Me aceptas?';
-        $sponsors_winwins->ww_accept = 1;
-        $sponsors_winwins->sponsor_accept = 1;
-        $sponsors_winwins->order = 2;
-        $sponsors_winwins->save();
-
-        $sponsors_winwins = new SponsorsWinwin();
-        $sponsors_winwins->winwin_id = 3;
-        $sponsors_winwins->sponsor_id = 1;
-        $sponsors_winwins->sponsor_message = 'Me aceptas?';
-        $sponsors_winwins->ww_accept = 1;
-        $sponsors_winwins->sponsor_accept = 1;
-        $sponsors_winwins->order = 4;
-        $sponsors_winwins->save();
-
-        $sponsors_winwins = new SponsorsWinwin();
-        $sponsors_winwins->winwin_id = 4;
-        $sponsors_winwins->sponsor_id = 1;
-        $sponsors_winwins->ww_message = 'Me aceptas?';
-        $sponsors_winwins->ww_accept = 1;
-        $sponsors_winwins->sponsor_accept = 1;
-        $sponsors_winwins->order = 3;
-        $sponsors_winwins->save();
-
-        $sponsors_winwins = new SponsorsWinwin();
-        $sponsors_winwins->winwin_id = 5;
-        $sponsors_winwins->sponsor_id = 1;
-        $sponsors_winwins->sponsor_message = 'Me aceptas?';
-        $sponsors_winwins->ww_accept = 0;
-        $sponsors_winwins->sponsor_accept = 1;
-        $sponsors_winwins->save();
-
-        $sponsors_winwins = new SponsorsWinwin();
-        $sponsors_winwins->winwin_id = 6;
-        $sponsors_winwins->sponsor_id = 1;
-        $sponsors_winwins->ww_message = 'Me aceptas?';
-        $sponsors_winwins->ww_accept = 1;
-        $sponsors_winwins->sponsor_accept = 0;
-        $sponsors_winwins->save();
-
 
         $sponsor = new Sponsor();
-        $sponsor->name = 'Ford';
-        $sponsor->user_id = 100;
-        $sponsor->about = 'Ford - Find your way';
-        $sponsor->contact_name = 'Mr. Ford';
-        $sponsor->contact_phone = '22222222';
-        $sponsor->contact_email = 'info@ford.com';
-        $sponsor->type = 'Automotriz';
-        $sponsor->cover_photo = 'ford.png';
+        $sponsor->name = 'Otro Mundo';
+        $sponsor->user_id = 1;
+        $sponsor->about = 'Otro Mundo';
+        $sponsor->contact_name = 'Claudio';
+        $sponsor->contact_phone = '+541143816792';
+        $sponsor->contact_email = 'clodo@camba.coop';
+        $sponsor->type = 'Comercial';
+        $sponsor->cover_photo = 'otromundo-sponsor.png';
         $sponsor->is_active = 1;
         $sponsor->is_main = 1;
         $sponsor->status = 'ACTIVE';
-        $sponsor->photo = 'ford.jpg';
+        $sponsor->photo = 'otromundo-sponsor.png';
+
         $sponsor->save();
 
-*/
+        $sponsor = new Sponsor();
+        $sponsor->name = 'Humanet';
+        $sponsor->user_id = 1;
+        $sponsor->about = 'Humanet';
+        $sponsor->contact_name = 'Claudio';
+        $sponsor->contact_phone = '+541143816792';
+        $sponsor->contact_email = 'clodo@camba.coop';
+        $sponsor->type = 'Comercial';
+        $sponsor->cover_photo = 'humanet-sponsor.png';
+        $sponsor->is_active = 1;
+        $sponsor->is_main = 1;
+        $sponsor->status = 'ACTIVE';
+        $sponsor->photo = 'humanet-sponsor.png';
 
+        $sponsor->save();
 
+        $sponsor = new Sponsor();
+        $sponsor->name = 'CORBO';
+        $sponsor->user_id = 1;
+        $sponsor->about = 'CORBO';
+        $sponsor->contact_name = 'Claudio';
+        $sponsor->contact_phone = '+541143816792';
+        $sponsor->contact_email = 'clodo@camba.coop';
+        $sponsor->type = 'Comercial';
+        $sponsor->cover_photo = 'corbo-sponsor.png';
+        $sponsor->is_active = 1;
+        $sponsor->is_main = 1;
+        $sponsor->status = 'ACTIVE';
+        $sponsor->photo = 'corbo-sponsor.png';
+
+        $sponsor->save();
+
+        $sponsor = new Sponsor();
+        $sponsor->name = 'Chicas Poderosas';
+        $sponsor->user_id = 1;
+        $sponsor->about = 'Chicas Poderosas';
+        $sponsor->contact_name = 'Claudio';
+        $sponsor->contact_phone = '+541143816792';
+        $sponsor->contact_email = 'clodo@camba.coop';
+        $sponsor->type = 'Comercial';
+        $sponsor->cover_photo = 'chicas-sponsor.png';
+        $sponsor->is_active = 1;
+        $sponsor->is_main = 1;
+        $sponsor->status = 'ACTIVE';
+        $sponsor->photo = 'chicas-sponsor.png';
+
+        $sponsor->save();
+
+        $sponsor = new Sponsor();
+        $sponsor->name = 'Cambá Laboratorio de Tecnología';
+        $sponsor->user_id = 1;
+        $sponsor->about = 'Cambá Laboratorio de Tecnología';
+        $sponsor->contact_name = 'Claudio';
+        $sponsor->contact_phone = '+541143816792';
+        $sponsor->contact_email = 'clodo@camba.coop';
+        $sponsor->type = 'Comercial';
+        $sponsor->cover_photo = 'camba-sponsor.png';
+        $sponsor->is_active = 1;
+        $sponsor->is_main = 1;
+        $sponsor->status = 'ACTIVE';
+        $sponsor->photo = 'camba-sponsor.png';
+
+        $sponsor->save();
+
+        $sponsor = new Sponsor();
+        $sponsor->name = 'Perian SA';
+        $sponsor->user_id = 1;
+        $sponsor->about = 'Perian SA';
+        $sponsor->contact_name = 'Claudio';
+        $sponsor->contact_phone = '+541143816792';
+        $sponsor->contact_email = 'clodo@camba.coop';
+        $sponsor->type = 'Comercial';
+        $sponsor->cover_photo = 'perian-sponsor.png';
+        $sponsor->is_active = 1;
+        $sponsor->is_main = 0;
+        $sponsor->status = 'ACTIVE';
+        $sponsor->photo = 'perian-sponsor.png';
+
+        $sponsor->save();
+
+        $sponsor = new Sponsor();
+        $sponsor->name = 'Otro Mundo';
+        $sponsor->user_id = 1;
+        $sponsor->about = 'Otro Mundo';
+        $sponsor->contact_name = 'Claudio';
+        $sponsor->contact_phone = '+541143816792';
+        $sponsor->contact_email = 'clodo@camba.coop';
+        $sponsor->type = 'Comercial';
+        $sponsor->cover_photo = 'otromundo-sponsor.png';
+        $sponsor->is_active = 1;
+        $sponsor->is_main = 0;
+        $sponsor->status = 'ACTIVE';
+        $sponsor->photo = 'otromundo-sponsor.png';
+
+        $sponsor->save();
+
+        $sponsor = new Sponsor();
+        $sponsor->name = 'Humanet';
+        $sponsor->user_id = 1;
+        $sponsor->about = 'Humanet';
+        $sponsor->contact_name = 'Claudio';
+        $sponsor->contact_phone = '+541143816792';
+        $sponsor->contact_email = 'clodo@camba.coop';
+        $sponsor->type = 'Comercial';
+        $sponsor->cover_photo = 'humanet-sponsor.png';
+        $sponsor->is_active = 1;
+        $sponsor->is_main = 0;
+        $sponsor->status = 'ACTIVE';
+        $sponsor->photo = 'humanet-sponsor.png';
+
+        $sponsor->save();
+
+        $sponsor = new Sponsor();
+        $sponsor->name = 'CORBO';
+        $sponsor->user_id = 1;
+        $sponsor->about = 'CORBO';
+        $sponsor->contact_name = 'Claudio';
+        $sponsor->contact_phone = '+541143816792';
+        $sponsor->contact_email = 'clodo@camba.coop';
+        $sponsor->type = 'Comercial';
+        $sponsor->cover_photo = 'corbo-sponsor.png';
+        $sponsor->is_active = 1;
+        $sponsor->is_main = 0;
+        $sponsor->status = 'ACTIVE';
+        $sponsor->photo = 'corbo-sponsor.png';
+
+        $sponsor->save();
+
+        $sponsor = new Sponsor();
+        $sponsor->name = 'Chicas Poderosas';
+        $sponsor->user_id = 1;
+        $sponsor->about = 'Chicas Poderosas';
+        $sponsor->contact_name = 'Claudio';
+        $sponsor->contact_phone = '+541143816792';
+        $sponsor->contact_email = 'clodo@camba.coop';
+        $sponsor->type = 'Comercial';
+        $sponsor->cover_photo = 'chicas-sponsor.png';
+        $sponsor->is_active = 1;
+        $sponsor->is_main = 0;
+        $sponsor->status = 'ACTIVE';
+        $sponsor->photo = 'chicas-sponsor.png';
+
+        $sponsor->save();
+
+        $sponsor = new Sponsor();
+        $sponsor->name = 'Cambá Laboratorio de Tecnología';
+        $sponsor->user_id = 1;
+        $sponsor->about = 'Cambá Laboratorio de Tecnología';
+        $sponsor->contact_name = 'Claudio';
+        $sponsor->contact_phone = '+541143816792';
+        $sponsor->contact_email = 'clodo@camba.coop';
+        $sponsor->type = 'Comercial';
+        $sponsor->cover_photo = 'camba-sponsor.png';
+        $sponsor->is_active = 1;
+        $sponsor->is_main = 0;
+        $sponsor->status = 'ACTIVE';
+        $sponsor->photo = 'camba-sponsor.png';
+
+        $sponsor->save();
     }
 
 }
