4f3a2416849c0055fd657998d533ea369468935a
jeronimo <jeronimocoello@gmail.com>
2016-07-01 15:14:23 -0300

    Agrega migraciones para grupos fix

--- backend/database/migrations/2016_01_07_add_cancelled_reason_to_group.php
+++ backend/database/migrations/2016_01_07_add_cancelled_reason_to_group.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class addCanceledReasonToGroup extends Migration
+class AddCanceledReasonToGroup extends Migration
 {
     /**
      * Run the migrations.
