b7351450749051640aae1b2406897390f024110a
Agustin Barg <agusbarg@hotmail.com>
2016-10-09 18:55:37 -0300

    chat p2p, directiva inbox con modal a terminar

--- frontend/src/app/profile/public-profile.controller.js
+++ frontend/src/app/profile/public-profile.controller.js
@@ -130,6 +130,17 @@
     vm.sendMessage = function(convid, msj){
       user.sendMessage({ conversation_id: convid, message: msj, receiver_id: vm.userId, subject : 'asunto new conversation'})
     }
+
+      vm.inbox = function () {
+          $mdDialog.show({
+              controller: function () {
+                  return self;
+              },
+              controllerAs: 'PublicProfileController',
+              templateUrl: 'app/profile/modal-inbox.tmpl.html',
+              clickOutsideToClose: true
+          });
+      };
   }
 
 
