3d3b94d529bfc27bd2e07766a4fb0bb0ac8bb942
Miguel Martinez Soler <miguelmsoler@gmail.com>
2016-11-25 21:23:08 -0300

    Agrego validación para textarea de mensaje Ahora no se puede enviar mensajes vacíos o llenos de espacios

--- frontend/src/app/profile/public-profile.controller.js
+++ frontend/src/app/profile/public-profile.controller.js
@@ -314,20 +314,21 @@
     vm.sendMessageStatus = "notSended";
     vm.conversationId = conversation_id;
 
-        vm.sendMessage = function(){
-      user.sendMessage({
-        conversation_id: vm.conversationId,
-        message: vm.mensaje,
-        receiver_id: vm.toUserId,
-        subject : 'asunto new conversation'
-      }).then(function(data){
-        if(data[0]=='enviado'){
-          vm.sendMessageStatus = "Sended";
-          $timeout(function() {
-            $mdDialog.hide(data);
-            }, 3000);
-        }
-      });
+    vm.sendMessage = function(){
+      if (vm.mensaje.trim().lenght>0)
+        user.sendMessage({
+          conversation_id: vm.conversationId,
+          message: vm.mensaje,
+          receiver_id: vm.toUserId,
+          subject : 'asunto new conversation'
+        }).then(function(data){
+          if(data[0]=='enviado'){
+            vm.sendMessageStatus = "Sended";
+            $timeout(function() {
+              $mdDialog.hide(data);
+              }, 3000);
+          }
+        });
     }
   }
 
